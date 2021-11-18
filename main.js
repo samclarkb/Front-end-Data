fe8f3f03b5f61526235e2a84cf28d91679391f03
samclarkb <samclarkboot@planet.nl>
2021-11-18 11:42:16 +0100

    functional programming en es6

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const margin = { top: 40, bottom: 10, left: 120, right: 20 }
 const width = 1200 - margin.left - margin.right
 const height = 600 - margin.top - margin.bottom
 
-//
+// Aanmaken en het stijlen van de SVG
 const svg = d3
-	.select('body')
-	.append('svg')
+	.select('body') // body wordt geselcteerd
+	.append('svg') // SVG wordty aangemaakt
 	.attr('width', width + margin.left + margin.right)
 	.attr('height', height + margin.top + margin.bottom)
 
@@ -26,33 +26,29 @@ const g_yaxis = g.append('g').attr('class', 'y axis')
 // Hier haal ik data uit de externe API
 d3.json(
 	'https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&limit=10&api_key=05064fdc55f8c3320ca9ed2c12ae1fa4&artist?&format=json'
-).then(json => {
-	data = json.artists.artist // Ik zet artists.artist achter json om het pad van het object te definiëren
+)
+	.then(json => {
+		data = json.artists.artist // Ik zet artists.artist achter json om het pad van het object te definiëren
 
-	update(data).catch(err => {
+		update(data)
+	})
+	.catch(err => {
 		console.error(err)
 	})
-})
 
-function update(data, type) {
-	sorteren(data, type)
-
-	if (type === 'playcount') {
-		xscale.domain([0, d3.max(data, d => +d.playcount)])
-	} else if (type === 'listeners') {
-		xscale.domain([0, d3.max(data, d => +d.listeners)])
-	} else if (type === 'average') {
-		xscale.domain([0, d3.max(data, d => +d.playcount / d.listeners)])
-	} else {
-		xscale.domain([0, d3.max(data, d => +d.playcount)])
-	}
-	yscale.domain(data.map(d => d.name))
+// Hier roep ik alle losse functies aan
+const update = (data, type) => {
+	sorteren(data, type) // Hier roep ik de sorteren functie aan
+	assenUpdate(data, type) // Hier roep ik de functie aan die ervoor zorgt dat X en de Y as worden geupdatet
+	vierkant(type) // Deze functie zorgt ervoor dat de data wordt vertuurd naar de rectangle
 
 	// Het renderen van de X en Y as. Ook geef ik hier een transitie van één seconde mee
 	g_xaxis.transition().duration(1000).ease(d3.easePoly).call(xaxis)
 	g_yaxis.transition().duration(1000).ease(d3.easePoly).call(yaxis)
+}
 
-	//
+// functie om de data in de rectangles te stoppen
+vierkant = type => {
 	const rect = g
 		.selectAll('rect')
 		.data(data, d => d.name)
@@ -61,11 +57,10 @@ function update(data, type) {
 			rect_enter.append('title')
 			return rect_enter
 		})
-		// aanroepen van de mouse events
-		.on('mouseover', onMouseOver)
-		.on('mousemove', onMouseOver) // Mousemove returnt constant de coördinaten van de muis
-		.on('mouseout', onMouseOut)
 
+		// aanroepen van de mouse events
+		.on('mousemove', onMouseMove) // Mousemove returnt constant de coördinaten van de muis
+		.on('mouseout', onMouseOut)
 	rect.attr('height', yscale.bandwidth())
 
 	// hier stop ik de data in de rectangles. Ik geef hier ook de transities op mijn rectangles mee
@@ -94,7 +89,7 @@ function update(data, type) {
 
 // Dit is de functie die ervoor zorgt dat de artiest met de hoogste waarde bovenaan de grafiek staat
 sorteren = (data, type) => {
-	data.sort(function (a, b) {
+	data.sort((a, b) => {
 		if (type === 'playcount') {
 			return b.playcount - a.playcount // b - a omdat ik hoogste waarde bovenaan wil zetten. Als ik de laagste waarde bovenaan wil zetten wordt het a - b
 		} else if (type === 'listeners') {
@@ -107,8 +102,22 @@ sorteren = (data, type) => {
 	})
 }
 
+// Hier geef ik data mee aan de X en de Y as
+assenUpdate = (data, type) => {
+	if (type === 'playcount') {
+		xscale.domain([0, d3.max(data, d => +d.playcount)])
+	} else if (type === 'listeners') {
+		xscale.domain([0, d3.max(data, d => +d.listeners)])
+	} else if (type === 'average') {
+		xscale.domain([0, d3.max(data, d => +d.playcount / d.listeners)])
+	} else {
+		xscale.domain([0, d3.max(data, d => +d.playcount)])
+	}
+	yscale.domain(data.map(d => d.name))
+}
+
 // Deze functie zorgt ervoor dat er een pop up verschijnt wanneer je over een rectangle heen hovert
-function onMouseOver(d, data) {
+const onMouseMove = (d, data) => {
 	// d.clientX en d.clientY zijn properties in het onMouseOver object die de coördinaten van de muis opspoort
 	const xPosition = d.clientX
 	const yPosition = d.clientY
@@ -120,12 +129,13 @@ function onMouseOver(d, data) {
 		toolTipValue = data[selection]
 	}
 
-	d3.select(this).attr('class', 'highlight') // highlight refereert naar een class in de CSS die de opacity op 0,7 zet
+	d3.select(d.target).attr('class', 'highlight') // highlight refereert naar een class in de CSS die de opacity op 0,7 zet
 	d3.select('#tooltip').classed('hidden', false) // ik zet de CSS class hidden op false, zodat de pop te voorschijn komt
 	d3.select('#tooltip') // hier geef ik aan dat de pop up op de coördinaten van de muis moet worden weergegeven
 		.style('left', xPosition + 'px')
 		.style('top', yPosition + 'px')
 	d3.select('#name').text(`${data.name} heeft `)
+
 	// Ik gebruik hier een if else statement, omdat ik voor iedere filter een andere text wil laten zien
 	if (selection === 'playcount') {
 		d3.select('#value').text(`${toolTipValue} totaal aantal streams `)
@@ -139,8 +149,8 @@ function onMouseOver(d, data) {
 }
 
 // Deze functie zorgt ervoor dat de pop up wordt verwijderd wanneer de mui zich niet meer op de rectangle bevindt
-function onMouseOut() {
-	d3.select(this).attr('class', 'bar')
+const onMouseOut = d => {
+	d3.select(d.target).attr('class', 'bar')
 	d3.select('#tooltip').classed('hidden', true) // Hidden refereert naar een CSS class die de pop up op display none zet
 }
 
@@ -148,19 +158,20 @@ function onMouseOut() {
 let selection = 'playcount'
 
 // Met deze functie leg ik de verbinding tussen de radio buttons en de data uit de externe API
-d3.selectAll('#filter').on('change', function () {
-	const checked = d3.select(this).property('checked')
+d3.selectAll('#filter').on('change', i => {
+	const target = i.target
+	const checked = d3.select(target).property('checked')
 	if (checked === true) {
-		if (d3.select(this).node().value === 'playcount') {
+		if (d3.select(target).node().value === 'playcount') {
 			// Er wordt hier gerefereert naar de value van de radio buttons
 			selection = 'playcount'
 			update(data, 'playcount')
 		}
-		if (d3.select(this).node().value === 'listeners') {
+		if (d3.select(target).node().value === 'listeners') {
 			selection = 'listeners'
 			update(data, 'listeners')
 		}
-		if (d3.select(this).node().value === 'average') {
+		if (d3.select(target).node().value === 'average') {
 			selection = 'average'
 			update(data, 'average')
 		}
